6e1ae6052648adc69ac7abeea47439fb9d20999e
tfs-apps-one <furu12080205@gmail.com>
2022-08-13 15:56:41 +0900

    画面デザイン一新

--- app/src/main/java/ftapps/goodshopping/MainActivity.java
+++ app/src/main/java/ftapps/goodshopping/MainActivity.java
@@ -1,5 +1,6 @@
 package ftapps.goodshopping;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,26 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void onCalExec(View view){
+
+    }
+    //  メニュー
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    //  メニュー選択時の処理
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -48,9 +42,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent1 = new Intent(this, GsSetActivity.class);
+            startActivity(intent1);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
